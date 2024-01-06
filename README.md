b8fe14c3778ad3bdea34d9f513bdcde792d54130
Enchan <enchant0130@gmail.com>
2024-01-06 20:53:45 +0900

    [Add] initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# mazegen
+
+## Overview
+
+Automatic maze generation
+
+## License
+
+This repository is published under [MIT License](LICENSE).
---
+++ src/main.py
@@ -0,0 +1,13 @@
+#
+# 迷路自動生成
+#
+import sys
+import mazegen
+
+
+def main() -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
